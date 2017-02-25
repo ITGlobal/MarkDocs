7224cdde5ef948704a5d81da15aa89e9d7b134de
Albert Kapitanov <kapitanov@itglobal.ru>
2017-02-25 19:13:07 +0300

    Added support for server-side syntax colorization

---
+++ src/ITGlobal.MarkDocs.Markdown/Format/ColorCodeSyntaxColorizer.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Linq;
+using ColorCode;
+using ColorCode.Formatting;
+using ColorCode.Styling.StyleSheets;
+using JetBrains.Annotations;
+
+namespace ITGlobal.MarkDocs.Format
+{
+    /// <summary>
+    ///     A server-side syntax colorizer based on ColorCode library
+    /// </summary>
+    [PublicAPI]
+    public sealed class ColorCodeSyntaxColorizer : ISyntaxColorizer
+    {
+        private static readonly HtmlFormatter Formatter = new HtmlFormatter();
+        private static readonly DefaultStyleSheet StyleSheet = new DefaultStyleSheet();
+
+        /// <summary>
+        ///     List of supported languages
+        /// </summary>
+        public string[] SupportedLanguages { get; } = Languages.All.Select(_ => _.Id).ToArray();
+
+        /// <summary>
+        ///     Render a source code into an HTML
+        /// </summary>
+        public string Render(string languageId, string sourceCode)
+        {
+            var language = Languages.FindById(languageId);
+            if (language == null)
+            {
+                return $"<pre><code class=\"{languageId}\">{sourceCode}</code></pre>";
+            }
+
+            var colorizer = new CodeColorizer();
+            using (var writer = new StringWriter())
+            {
+                colorizer.Colorize(sourceCode, language, Formatter, StyleSheet, writer);
+                var html = writer.ToString();
+                return html;
+            }
+        }
+    }
+}
