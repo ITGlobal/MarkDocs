78953fa8c409fd8123caef47053a9dfda8950512
Albert Kapitanov <kapitanov@itglobal.ru>
2017-03-20 11:01:36 +0300

    #20 Change TOC format to plain bullet list

--- src/ITGlobal.MarkDocs.Markdown/Format/TableOfContents/TableOfContentsBlockRenderer.cs
+++ src/ITGlobal.MarkDocs.Markdown/Format/TableOfContents/TableOfContentsBlockRenderer.cs
@@ -16,25 +16,74 @@ namespace ITGlobal.MarkDocs.Format.TableOfContents
             {
                 var document = block.Document;
 
-                var headers = document.OfType<HeadingBlock>().ToArray();
-
-                renderer.WriteLine("<div class=\"panel panel-primary\">");
-                renderer.WriteLine("  <div class=\"panel-heading\">Contents</div>");
-                renderer.WriteLine("  <div class=\"list-group\">");
-                foreach (var header in headers)
-                {
-                    var id = header.GetAttributes().Id;
-                    var text = header.Inline.GetText();
-
-                    renderer.Write($"   <a href=\"#{id}\" class=\"list-group-item\" style=\"margin-left: {header.Level * 10}px\">{text}</a>");
-                }
-                renderer.WriteLine("  </div>");
-                renderer.WriteLine("</div>");
+                var headings = document.OfType<HeadingBlock>().ToArray();
+                var i = 0;
+                RenderTree(renderer, headings, ref i, 1);
             }
             catch (Exception exception)
             {
                 Logger.LogError(0, exception, "Error while rendering {0}", nameof(TableOfContentsBlock));
                 renderer.WriteError("Failed to render table of contents");
+            }
+        }
+
+        private static void RenderTree(HtmlRenderer renderer, HeadingBlock[] headings, ref int i, int currentLevel)
+        {
+            var hasAnyItems = false;
+            var closeLi = false;
+            var skipFirstL1 = true;
+
+            for (; i < headings.Length; i++)
+            {
+                var h = headings[i];
+
+                if (h.Level > currentLevel)
+                {
+                    RenderTree(renderer, headings, ref i, currentLevel + 1);
+                    continue;
+                }
+
+                if (h.Level < currentLevel)
+                {
+                    break;
+                }
+
+                if (h.Level == 1 && skipFirstL1)
+                {
+                    // First H1 is a page title and should not be a part of TOC
+                    skipFirstL1 = false;
+                    continue;
+                }
+                
+                if (!hasAnyItems)
+                {
+                    renderer.WriteLine("<ul>");
+                    hasAnyItems = true;
+                }
+
+                if (closeLi)
+                {
+                    renderer.WriteLine("</li>");
+                    closeLi = false;
+                }
+
+                renderer.WriteLine("<li>");
+                var id = h.GetAttributes().Id;
+                var text = h.Inline.GetText();
+
+                renderer.Write($"<a href=\"#{id}\">{text}</a>");
+
+                closeLi = true;
+            }
+
+            if (closeLi)
+            {
+                renderer.WriteLine("</li>");
+            }
+
+            if (hasAnyItems)
+            {
+                renderer.WriteLine("</ul>");
             }
         }
     }
