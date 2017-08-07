907b59fc6188a9d7e92986a5e471b3f1fd859f1f
Albert Kapitanov <kapitanov@itglobal.ru>
2017-08-07 12:19:18 +0300

    Added admonition blocks

---
+++ src/ITGlobal.MarkDocs.Markdown/Format/Admonition/AdmonitionExtension.cs
@@ -0,0 +1,22 @@
+﻿using Markdig;
+using Markdig.Renderers;
+
+namespace ITGlobal.MarkDocs.Markdown.Format.Admonition
+{
+    internal sealed class AdmonitionExtension : IMarkdownExtension
+    {
+        public void Setup(MarkdownPipelineBuilder pipeline)
+        {
+            pipeline.BlockParsers.Insert(0, new AdmonitionParser());
+        }
+
+        public void Setup(MarkdownPipeline pipeline, IMarkdownRenderer renderer)
+        {
+            var htmlRenderer = renderer as HtmlRenderer;
+            if (htmlRenderer != null)
+            {
+                htmlRenderer.ObjectRenderers.AddIfNotAlready<AdmonitionRenderer>();
+            }
+        }
+    }
+}
