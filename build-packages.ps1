ec48dbb9d4a28bb46ace0ba5a48dc2e29ea38196
Albert Kapitanov <kapitanov@itglobal.ru>
2017-08-08 11:10:16 +0300

    Fixed build for NET46

---
+++ build-packages.ps1
@@ -0,0 +1,47 @@
+$SOLUTION_DIR = Resolve-Path "."
+$ARTIFACTS = Join-Path $SOLUTION_DIR "artifacts"
+$CONFIGURATION = "Release"
+$VERSION = "0.0.0-dev"
+
+# init
+if(-not (Test-Path $ARTIFACTS)) {
+    New-Item -Path $ARTIFACTS -ItemType Directory | Out-Null    
+}
+
+# clean
+Get-ChildItem -Path $ARTIFACTS | Remove-Item -Force -Recurse
+Get-ChildItem -Filter "bin" -Directory -Recurse | Remove-Item -Force -Recurse
+Get-ChildItem -Filter "obj" -Directory -Recurse | Remove-Item -Force -Recurse
+
+# version
+$gitVersion = $(git tag) | ? { $_ -match "v([0-9]+\.[0-9]+)" } | Select -Last 1 | % { $_.Substring(1) }
+$buildNumber = 0
+if($env:APPVEYOR) {
+    $buildNumber = $env:APPVEYOR_BUILD_NUMBER
+}
+$branch = $(git rev-parse --abbrev-ref HEAD).Replace("/", "").Replace("-", "").Replace("\\", "")
+$VERSION = $gitVersion + "." + $buildNumber
+if($branch -ne "master") {
+    $VERSION += "-$branch"
+}
+
+Write-Host "Version: $VERSION"
+
+# restore
+& dotnet restore /v:q /nologo /p:Version=$VERSION
+if($LASTEXITCODE -ne 0) {
+    Write-Host "'dotnet restore' exited with $LASTEXITCODE"
+    exit 1
+}
+
+# packages
+
+Get-ChildItem -Filter *.csproj -Path "./src" -Recurse -File | %{ 
+    & dotnet pack --output $ARTIFACTS -v q /nologo /p:Version=$VERSION /p:Configuration=$CONFIGURATION $_.FullName
+    if($LASTEXITCODE -ne 0) {
+        Write-Host "'dotnet pack' exited with $LASTEXITCODE"
+        exit 1
+    }
+
+    Write-Host "Compiled package $($_.Name)"
+}
