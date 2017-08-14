30ac034d687e78d9067770d93199ac8243556f92
Albert Kapitanov <kapitanov@itglobal.ru>
2017-08-14 18:08:25 +0300

    Fixed push-nuget script

--- push-nuget.ps1
+++ push-nuget.ps1
@@ -10,7 +10,7 @@ if (!$NUGET_API_KEY) {
 Get-ChildItem -Path $ARTIFACTS -Filter *.nupkg -File | % {
     $package = $_.FullName
     Write-Host "Pushing package $package"
-    & dotnet nuget push --api-key $NUGET_API_KEY --force-english-output $package
+    & dotnet nuget push --api-key $NUGET_API_KEY --force-english-output -s nuget.org $package
     if ($LASTEXITCODE -ne 0) {
         Write-Host "'dotnet nuget push' exited with $LASTEXITCODE"
         exit 1
